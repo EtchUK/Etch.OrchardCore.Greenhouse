d36b42220cb5101a79722fe75bb1946961e2ef73
Peter Keating <pete.keating@etchuk.com>
2021-07-22 10:10:30 +0100

    Move update4 migration to create recipe

--- Migrations.cs
+++ Migrations.cs
@@ -50,12 +50,5 @@ namespace Etch.OrchardCore.Greenhouse
 
             return 4;
         }
-
-        public async Task<int> UpdateFrom4Async()
-        {
-            await _recipeMigrator.ExecuteAsync("update4.recipe.json", this);
-
-            return 5;
-        }
     }
 }
