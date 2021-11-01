b517a34e05ac9c24f2aa2976bb30d3bcfdac5e6b
Peter Keating <pete.keating@etchuk.com>
2021-11-01 14:15:15 +0000

    Upgrade OrchardCore to 1.1.0

--- Manifest.cs
+++ Manifest.cs
@@ -5,7 +5,7 @@ using OrchardCore.Modules.Manifest;
     Category = "Content",
     Description = "Integrates Greenhouse with Orchard Core.",
     Name = "Greenhouse",
-    Version = "0.1.3",
+    Version = "0.2.0",
     Website = "https://etchuk.com"
 )]
 
