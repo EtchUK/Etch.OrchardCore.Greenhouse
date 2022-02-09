3b04fa6bfdf20ad664697838828f44b6713ac62c
Peter Keating <pete.keating@etchuk.com>
2022-02-09 18:42:24 +0000

    v0.6.0

--- Manifest.cs
+++ Manifest.cs
@@ -5,7 +5,7 @@ using OrchardCore.Modules.Manifest;
     Category = "Content",
     Description = "Integrates Greenhouse with Orchard Core.",
     Name = "Greenhouse",
-    Version = "0.5.1",
+    Version = "0.6.0",
     Website = "https://etchuk.com"
 )]
 
