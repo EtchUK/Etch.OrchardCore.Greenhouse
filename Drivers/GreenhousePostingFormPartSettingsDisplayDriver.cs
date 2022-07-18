4b12b793a062bdd99f4de1d01528515562c65202
Peter Keating <pete.keating@etchuk.com>
2022-07-18 11:39:00 +0100

    Add ability to use invisible recaptcha on application form
    If the ReCaptcha feature is enabled and configured, ReCaptcha can be
    enabled within the Greenhouse form part settings.

--- Drivers/GreenhousePostingFormPartSettingsDisplayDriver.cs
+++ Drivers/GreenhousePostingFormPartSettingsDisplayDriver.cs
@@ -2,21 +2,33 @@
 using Etch.OrchardCore.Greenhouse.ViewModels;
 using OrchardCore.ContentManagement.Metadata.Models;
 using OrchardCore.ContentTypes.Editors;
+using OrchardCore.DisplayManagement.Handlers;
 using OrchardCore.DisplayManagement.ModelBinding;
 using OrchardCore.DisplayManagement.Views;
+using OrchardCore.Environment.Shell;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Etch.OrchardCore.Greenhouse.Drivers
 {
     public class GreenhousePostingFormPartSettingsDisplayDriver : ContentTypePartDefinitionDisplayDriver
     {
-        public override IDisplayResult Edit(ContentTypePartDefinition contentTypePartDefinition, IUpdateModel updater)
+        private readonly IShellFeaturesManager _shellFeaturesManager;
+
+        public GreenhousePostingFormPartSettingsDisplayDriver(IShellFeaturesManager shellFeaturesManager)
+        {
+            _shellFeaturesManager = shellFeaturesManager;
+        }
+
+        public override async Task<IDisplayResult> EditAsync(ContentTypePartDefinition contentTypePartDefinition, IUpdateModel updater)
         {
             if (!string.Equals(nameof(GreenhousePostingFormPart), contentTypePartDefinition.PartDefinition.Name))
             {
                 return null;
             }
+
+            var enabledFeatures = await _shellFeaturesManager.GetEnabledExtensionsAsync();
 
             return Initialize<GreenhousePostingFormPartSettingsViewModel>("GreenhousePostingFormPartSettings_Edit", model =>
             {
@@ -25,9 +37,11 @@ namespace Etch.OrchardCore.Greenhouse.Drivers
                 model.AllowedFileExtensions = string.Join(",", settings.AllowedFileExtensions ?? Array.Empty<string>());
                 model.ApplicationErrorMessage = settings.ApplicationErrorMessage;
                 model.ApplicationSuccessUrl = settings.ApplicationSuccessUrl;
+                model.HasReCaptchaEnabled = enabledFeatures.Any(x => x.Id == "OrchardCore.ReCaptcha");
                 model.MaxFileSize = settings.MaxFileSize;
                 model.ShowApplicationForm = settings.ShowApplicationForm;
                 model.SubmitButtonLabel = settings.SubmitButtonLabel;
+                model.UseReCaptcha = settings.UseReCaptcha;
                 model.ValidationErrorsMessage = settings.ValidationErrorsMessage;
             }).Location("Content");
         }
@@ -51,10 +65,11 @@ namespace Etch.OrchardCore.Greenhouse.Drivers
                 MaxFileSize = model.MaxFileSize,
                 ShowApplicationForm = model.ShowApplicationForm,
                 SubmitButtonLabel = model.SubmitButtonLabel,
+                UseReCaptcha = model.UseReCaptcha,
                 ValidationErrorsMessage = model.ValidationErrorsMessage
             });
 
-            return Edit(contentTypePartDefinition, context.Updater);
+            return await EditAsync(contentTypePartDefinition, context.Updater);
         }
     }
 }
