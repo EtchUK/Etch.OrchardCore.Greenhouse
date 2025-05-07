0c348f6f7d7eebfe5f335dee039d0fe1cfe85b3e
Myles Broomes <myles.broomes@etchuk.com>
2025-05-07 10:37:12 +0100

    Ensure all fix branches trigger a build

--- Manifest.cs
+++ Manifest.cs
@@ -5,7 +5,7 @@ using OrchardCore.Modules.Manifest;
     Category = "Content",
     Description = "Integrates Greenhouse with Orchard Core.",
     Name = "Greenhouse",
-    Version = "0.4.4",
+    Version = "0.4.5",
     Website = "https://etchuk.com"
 )]
 
